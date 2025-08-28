ce4e1aeef5974a7b9a340641896c6ec81a92547d
bs+ <solthings27@gmail.com>
2025-08-28 09:23:12 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,1 +1,15 @@
+package exercicio;
+
+public class Carro extends Veiculo {
+
+    public Carro() {
+        super();  // chama o construtor da superclasse Veiculo
+    }
+
+    @Override
+    public void acelerar() {
+        System.out.println("Carro acelerando...");
+    }
+}
+
 
